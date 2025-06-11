460de9ab2277e930aff248e362462e68ad729cbe
Clinton Brito <clintoncsb@gmail.com>
2025-06-11 20:24:56 -0300

    feat: start new project

--- scripts.js
+++ scripts.js
@@ -1,66 +1,24 @@
-var products = [
+members = [
     {
-        "photo": "img/big-mac.png",
-        "name": "Big Mac",
-        "price": 5.99,
-        "active": false,
-        "quantity": 1
+        fname: 'John',
+        lname: 'Lennon',
+        instrument: 'Violão'
     },
     {
-        "photo": "img/mc-chicken.png",
-        "name": "Mc Chicken",
-        "price": 4.99,
-        "active": false,
-        "quantity": 1
-    },
-    {
-        "photo": "img/double-cb.png",
-        "name": "Double Cheese Burger",
-        "price": 2.99,
-        "active": false,
-        "quantity": 1
-    },
-    {
-        "photo": "img/fries.png",
-        "name": "Batata frita",
-        "price": 2.99,
-        "active": false,
-        "quantity": 1
-    },
-    {
-        "photo": "img/nuggets.png",
-        "name": "Mc Nuggets",
-        "price": 3.49,
-        "active": false,
-        "quantity": 1
-    },
-    {
-        "photo": "img/salad.png",
-        "name": "Salada",
-        "price": 2.79,
-        "active": false,
-        "quantity": 1
-    },
-    {
-        "photo": "img/cola.png",
-        "name": "Coca Cola",
-        "price": 1.99,
-        "active": false,
-        "quantity": 1
-    },
-    {
-        "photo": "img/lipton.png",
-        "name": "Ice Tea",
-        "price": 1.99,
-        "active": false,
-        "quantity": 1
-    },
-    {
-        "photo": "img/water.png",
-        "name": "Água",
-        "price": 1.49,
-        "active": false,
-        "quantity": 1
+        fname: 'George',
+        lname: 'Harrison',
+        instrument: 'Guitarra'
     }
-];
- 
+]
+
+const handlingForms = {
+
+    data() {
+        return {
+            members: window.members
+        }
+    },
+
+};
+
+Vue.createApp(handlingForms).mount('#app');
