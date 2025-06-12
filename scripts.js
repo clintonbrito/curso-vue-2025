f01283589070a98ccb6902bc728095cc58e43574
Clinton Brito <clintoncsb@gmail.com>
2025-06-12 17:05:26 -0300

    feat: refactor todo item toggle functionality and add local storage management

--- scripts.js
+++ scripts.js
@@ -14,14 +14,26 @@ const todosApp = {
                 this.newTodo = {
                     done: false
                 }
+                saveTodos();
             } else {
                 alert('Você não pode inserir uma tarefa vazia')
             }
         },
         clearAll: function() {
             this.todos = []
+            localStorage.removeItem("todos")
+        },
+        saveTodos: function() {
+            localStorage.setItem("todos", JSON.stringify(this.todos));
+        },
+        toggleTodoDone: function(todo) {
+            todo.done = !todo.done;
+            this.saveTodos();
         }
-    }
+    },
+    created() {
+        this.todos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : this.todos;
+    },
 };
 
 Vue.createApp(todosApp).mount('#app');
