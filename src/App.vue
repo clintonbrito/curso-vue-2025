9d7e63ce3e157e1242f407ea0afcc06b82b056a6
Clinton Brito <clintoncsb@gmail.com>
2025-06-16 13:37:08 -0300

    feat: add axios and vue-axios for API integration and implement quiz functionality in App component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+
+    <template v-if="this.question">
+
+      <h1 v-html="this.question"></h1>
+  
+      <template v-for="(answer, index) in this.answers" v-bind:key="index">
+        <input
+          type="radio"
+          name="options"
+          value="answer"
+          />
+        <label v-html="answer"></label><br />
+      </template>
+
+    </template>
+
+
+    <button class="send" type="button">Send</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    return {
+      question: [],
+      incorrectAnswers: [],
+      correctAnswer: [],
+    }
+  },
+  computed: {
+    answers() {
+      var answers = this.incorrectAnswers ? [...this.incorrectAnswers] : [];
+      answers.splice(Math.round(Math.random() * answers.length),0, this.correctAnswer);
+      return answers;
+    }
+  },
+  created() {
+    this.axios
+      .get('https://opentdb.com/api.php?amount=1&category=11')
+      .then((response) => {
+        this.question = response.data.results[0].question;
+        this.incorrectAnswers = response.data.results[0].incorrect_answers;
+        this.correctAnswer = response.data.results[0].correct_answer;
+        console.log(response.data.results[0]);
+      })
   }
 }
+
+// https://opentdb.com/api.php?amount=1&category=11
+
 </script>
 
 <style lang="scss">
@@ -21,6 +63,23 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  max-width: 960px;
 }
+
+input[type='radio'] {
+  margin: 12px 4px;
+}
+
+button.send {
+  margin-top: 12px;
+  height: 40px;
+  min-width: 120px;
+  padding: 0 16px;
+  color: #fff;
+  background-color: #1867c0;
+  border: 1px solid #1867c0;
+  cursor: pointer;
+}
+
 </style>
